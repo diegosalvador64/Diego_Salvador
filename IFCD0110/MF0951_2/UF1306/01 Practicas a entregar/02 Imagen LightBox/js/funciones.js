998e4b91f509657c7b81c761b583752cf20ab965
diego1 <diesalvado@yahoo.es>
2019-04-10 13:57:53 +0200

    cambios 10-4-2019

--- IFCD0110/MF0951_2/UF1306/01 Practicas a entregar/02 Imagen LightBox/js/funciones.js
+++ IFCD0110/MF0951_2/UF1306/01 Practicas a entregar/02 Imagen LightBox/js/funciones.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+// Obtener el modal
+function tratarModal(a) { //ojo, que le he metido un parámetro "a" si utilizo los id
+		var b = a-1;
+		var modal = document.getElementById('miModal' + a);
+					
+		// Obtener la imagen e insertarla en el modal - Usar el texto que aparece en "alt" como captura
+		var img = document.getElementById('miImagen' + a);
+		var modalImg = document.getElementById("img" + a); 
+		var captionText = document.getElementById("captura" + a);
+	    										
+		img.onclick = function() {
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+						
+			}
+
+		// Guardar el elemento <span> que cierra el modal
+			
+			var span = document.getElementsByClassName("cerrar")[b];
+
+		// Cuando el usuario clica en el aspa del <span>, se cierra el modal
+			span.onclick = function() { 
+			modal.style.display = "none";
+					}
+}
